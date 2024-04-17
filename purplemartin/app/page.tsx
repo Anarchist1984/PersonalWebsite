b2c10281c87bf99a3d60fcae449788bbce005c8e
lifeissad 21 <nerd2923@gmail.com>
2024-04-17 00:33:45 +0000

    Added Authentication, need to add proper signin page

--- purplemartin/app/page.tsx
+++ purplemartin/app/page.tsx
@@ -1,5 +1,6 @@
 import MainNavbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import { signIn } from "@/auth"
 
 export default function Home(){
 
@@ -7,6 +8,12 @@ export default function Home(){
     <main className="dark">
       <div className=" dark:bg-dark-background-950 bg-background-950">
         <MainNavbar />
+        <div><form
+      action={async () => {
+        "use server"
+        await signIn("google")}}>
+      <button type="submit">Signin with Google</button>
+    </form></div>
         <Footer />
       </div>
     </main>
