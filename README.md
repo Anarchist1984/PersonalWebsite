1f8c1ac460fec1312a98df09d3480fc72045a64b
lifeissad 21 <nerd2923@gmail.com>
2024-04-22 18:28:09 +0000

    Got Database working

--- README.md
+++
@@ -1,2 +0,0 @@
-# PersonalWebsite
-This is a personal website for me to show off all the projects and things I have done
---
+++ my-app/app/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0gGinG5Kl9TLQXT-IJopdZWhm6CbCYoE",
+  authDomain: "purplemartin-d7870.firebaseapp.com",
+  projectId: "purplemartin-d7870",
+  storageBucket: "purplemartin-d7870.appspot.com",
+  messagingSenderId: "1042255981620",
+  appId: "1:1042255981620:web:fc4782892e136c1b31ee61",
+  measurementId: "G-M45E5S53EV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
